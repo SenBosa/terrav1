8c63e943bf8fb562addf39df5a641c6bc7e50858
Seth Bujosa <sethbb@live.com>
2016-10-31 18:56:44 -0700

    Initial commit

---
+++ Source/terra/terraGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "terraGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERRA_API AterraGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
